225f07780696a750e257b2af03fbe9dde26a367f
Tyler Riddle <cardboardaardvark@gmail.com>
2018-05-23 22:00:08 -0700

    started on a way to get control line changes

--- src/vserial.h
+++ src/vserial.h
@@ -8,14 +8,26 @@
 #ifndef SRC_VSERIAL_H_
 #define SRC_VSERIAL_H_
 
-struct _vserial_t;
+#include <stdbool.h>
 
 // VSERIAL * is supposed to be an opaque handle and the vserial interface
 // isn't tied to POSIX so this could in theory work on Windows
+struct _vserial_t;
 typedef struct _vserial_t VSERIAL;
+
+struct vserial_control_lines {
+    bool rts;
+    bool cts;
+    bool dtr;
+    bool dsr;
+};
+
+typedef int (*vserial_control_lines_handler)(struct vserial_control_lines *, void *);
 
 void vserial_destroy(VSERIAL *);
 VSERIAL * vserial_create(char *);
 char * vserial_get_name(VSERIAL *);
+
+void vserial_register_control_lines(VSERIAL *, vserial_control_lines_handler);
 
 #endif /* SRC_VSERIAL_H_ */
